a328f79b905a8a4a47c58519b8491891bbc97a24
Gary91002 <154485605+Gary91002@users.noreply.github.com>
2026-04-20 21:19:24 -0400

    added unit and integration tests for Vehicle Inventory API
    Added xUnit test projects and test cases for Vehicle Inventory
    - Unit tests for domain and service logic
    - Integration tests for all API endpoints

---
+++ Tests/G2VehicleInventory.WebAPI.IntegrationTests/BaseClasses/IntegrationTestBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace G2VehicleInventory.WebAPI.IntegrationTests.BaseClasses
+{
+	public class IntegrationTestBase : IClassFixture<WebApplicationFactory<Program>>
+	{
+		protected readonly HttpClient Client;
+
+		public IntegrationTestBase(WebApplicationFactory<Program> factory)
+		{
+			Client = factory.CreateClient();
+
+			// gateway middleware
+			Client.DefaultRequestHeaders.Add("X-From-Gateway", "GS-Gateway-Trusted-Token-111");
+		}
+	}
+}
